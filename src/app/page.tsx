2d094ca60a105f9531cf60849e9964927766342f
Ary Kiehr <ary@unixono.com>
2024-08-16 17:36:54 -0300

    Se agrega boton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,20 @@
-import { Box } from "@chakra-ui/react";
+"use client";
+
+import { Box, Button, ScaleFade } from "@chakra-ui/react";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Home() {
+  const [showImage, setShowImage] = useState(false);
   return (
     <>
-      <Box display="flex" justifyContent="center">
-        <Image src="/images/snupy.jpeg" width={500} height={500} alt="snupy" />
+      <Box display="flex" alignItems="center" justifyContent="center" height="100vh" py={15}>
+        <Button onClick={() => setShowImage(true)} size="lg" colorScheme="teal" hidden={showImage}>
+          Clickeame!
+        </Button>
+        <ScaleFade initialScale={0.9} in={showImage}>
+          <Image src="/images/snupy.jpeg" width={500} height={500} alt="snupy" hidden={!showImage} />
+        </ScaleFade>
       </Box>
     </>
   );
